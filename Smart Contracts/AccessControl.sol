95db389ab7d03bf523581d6b2aa7a05b9010ccce
camm2320 <n11090383@qut.edu.au>
2026-05-19 00:58:43 +1000

    Started setting up structure and smart contracts

---
+++ Smart Contracts/AccessControl.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.20;
+
+contract AccessControl {
+    
+    mapping(address => mapping(address => bool)) public access;
+
+    function grantAccess(address user) public {
+        access[msg.sender][user] = true;
+    }
+
+    function revokeAccess(address user) public {
+        access[msg.sender][user] = false;
+    }
+
+    function hasAccess(address patient, address user) public view returns (bool) {
+        return access[patient][user];
+    }
+}
